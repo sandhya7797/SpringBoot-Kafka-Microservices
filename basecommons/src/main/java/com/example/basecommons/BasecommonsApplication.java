ae79dbfdd321c620e036ec43bf471ab27dfc4a7b
Sandhya <ragulapadusandhya7797@gmail.com>
2025-01-11 12:35:24 -0800

    initial commit

---
+++ basecommons/src/main/java/com/example/basecommons/BasecommonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.basecommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasecommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasecommonsApplication.class, args);
+	}
+
+}
---
+++ basecommons/src/test/java/com/example/basecommons/BasecommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basecommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasecommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
